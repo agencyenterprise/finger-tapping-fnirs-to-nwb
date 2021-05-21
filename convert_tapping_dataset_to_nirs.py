be47300c79f243e0e513eddf86881bb9703a3d21
Darin Erat Sleiter <darin@ae.studio>
2021-05-21 16:13:12 +1000

    Run black to format code

--- convert_tapping_dataset_to_nirs.py
+++ convert_tapping_dataset_to_nirs.py
@@ -1,48 +1,51 @@
 import sys
-sys.path.append('src')
-
 import re
 from pathlib import Path
+
 from pynwb import NWBHDF5IO
 
+# todo compile conversion tools into an installable package
+sys.path.append("src")
 from snirf import load_snirf
 from nwb import convert_to_nwb
 
 
-DATASET_DIR = Path('data/BIDS-NIRS-Tapping/')
-
-
 def list_subj_dirs(dataset_dir):
-    subj_re = re.compile(r'^sub\-\d{2}$')
+    subj_re = re.compile(r"^sub\-\d{2}$")
 
     def is_subj_dir(path):
         return path.is_dir() and subj_re.match(path.name)
+
     subj_dirs = [path for path in dataset_dir.iterdir() if is_subj_dir(path)]
     return sorted(subj_dirs)
 
 
 def convert_subject_snirf_to_nwb(subj_dir):
     subj_id = subj_dir.name
-    snirf_path = subj_dir / 'nirs' / f'{subj_id}_task-tapping_nirs.snirf'
-    nwb_path = subj_dir / 'nirs' / f'{snirf_path.stem}.nwb'
+    snirf_path = subj_dir / "nirs" / f"{subj_id}_task-tapping_nirs.snirf"
+    nwb_path = subj_dir / "nirs" / f"{snirf_path.stem}.nwb"
 
-    print('converting SNIRF --> NWB:')
-    print(f'  {snirf_path} --> {nwb_path}')
+    print("converting SNIRF --> NWB:")
+    print(f"  {snirf_path} --> {nwb_path}")
 
     nwb = convert_to_nwb(
         load_snirf(snirf_path),
         file_identifier=snirf_path.stem,
-        session_description=f'{subj_id} NIRS recording data for a finger-tapping task',
-        manufacturer='SNIRF',
-        nirs_mode='continuous-wave'
+        session_description=f"{subj_id} NIRS recording data for a finger-tapping task",
+        manufacturer="SNIRF",
+        nirs_mode="continuous-wave",
     )
 
-    with NWBHDF5IO(str(nwb_path), 'w') as io:
+    with NWBHDF5IO(str(nwb_path), "w") as io:
         io.write(nwb)
 
 
-if __name__ == '__main__':
-    print('converting all snirf files in the dataset to nwb')
-    print(f'dataset directory: {DATASET_DIR}')
+DATASET_DIR = Path("data/BIDS-NIRS-Tapping/")
+
+
+# todo use click and add command line arguments and help
+if __name__ == "__main__":
+    print("converting all snirf files in the dataset to nwb")
+    print(f"dataset directory: {DATASET_DIR}")
     for subj_dir in list_subj_dirs(DATASET_DIR):
         convert_subject_snirf_to_nwb(subj_dir)
--- src/nwb.py
+++ src/nwb.py
@@ -1,35 +1,57 @@
 from hdmf.common import DynamicTableRegion
 from pynwb import NWBFile
 from pynwb.file import Subject
-from ndx_nirs import (NIRSDevice, NIRSSeries, NIRSChannelsTable,
-                      NIRSSourcesTable, NIRSDetectorsTable)
+from ndx_nirs import (
+    NIRSDevice,
+    NIRSSeries,
+    NIRSChannelsTable,
+    NIRSSourcesTable,
+    NIRSDetectorsTable,
+)
 
-from snirf import (extract_source_labels, extract_source_pos, extract_detector_labels,
-                   extract_detector_pos, extract_channels, extract_wavelengths,
-                   get_snirf_timestamps, get_snirf_data, get_session_datetime, get_subject_id)
+from snirf import (
+    extract_source_labels,
+    extract_source_pos,
+    extract_detector_labels,
+    extract_detector_pos,
+    extract_channels,
+    extract_wavelengths,
+    get_snirf_timestamps,
+    get_snirf_data,
+    get_session_datetime,
+    get_subject_id,
+)
 
 
-def convert_to_nwb(snirf, 
-                   file_identifier='N/A',
-                   session_description='not available',
-                   manufacturer='unknown',
-                   nirs_mode='continuous-wave'):
-    sources = compile_sources_table(extract_source_labels(snirf), extract_source_pos(snirf))
-    detectors = compile_detectors_table(extract_detector_labels(snirf), extract_detector_pos(snirf))
+def convert_to_nwb(
+    snirf,
+    file_identifier="N/A",
+    session_description="not available",
+    manufacturer="unknown",
+    nirs_mode="continuous-wave",
+):
+    sources = compile_sources_table(
+        extract_source_labels(snirf), extract_source_pos(snirf)
+    )
+    detectors = compile_detectors_table(
+        extract_detector_labels(snirf), extract_detector_pos(snirf)
+    )
 
     channel_meta = extract_channels(snirf)
     wavelengths = extract_wavelengths(snirf)
     channels = compile_channels_table(channel_meta, sources, detectors, wavelengths)
 
     device = compile_device(channels, manufacturer, nirs_mode)
-    nirs_series = compile_series(get_snirf_timestamps(snirf), get_snirf_data(snirf), channels)
-    
+    nirs_series = compile_series(
+        get_snirf_timestamps(snirf), get_snirf_data(snirf), channels
+    )
+
     nwb = NWBFile(
         session_description=session_description,
         identifier=file_identifier,
         session_start_time=get_session_datetime(snirf),
         subject=Subject(subject_id=get_subject_id(snirf)),
-        devices=[device]
+        devices=[device],
     )
     nwb.add_acquisition(nirs_series)
     return nwb
@@ -38,28 +60,28 @@ def convert_to_nwb(snirf,
 def compile_sources_table(labels, positions):
     table = NIRSSourcesTable()
     for label, pos in zip(labels, positions):
-        table.add_row({'label': label, 'x': pos[0], 'y': pos[1], 'z': pos[2]})
+        table.add_row({"label": label, "x": pos[0], "y": pos[1], "z": pos[2]})
     return table
 
 
 def compile_detectors_table(labels, positions):
     table = NIRSDetectorsTable()
     for label, pos in zip(labels, positions):
-        table.add_row({'label': label, 'x': pos[0], 'y': pos[1], 'z': pos[2]})
+        table.add_row({"label": label, "x": pos[0], "y": pos[1], "z": pos[2]})
     return table
 
 
 def compile_channels_table(channels_meta, sources, detectors, wavelengths):
     table = NIRSChannelsTable()
     for channel_id, channel in channels_meta.items():
-        source_label = sources.label[channel['source_idx']]
-        detector_label = detectors.label[channel['detector_idx']]
-        source_wavelength = wavelengths[channel['wavelength_idx']]
+        source_label = sources.label[channel["source_idx"]]
+        detector_label = detectors.label[channel["detector_idx"]]
+        source_wavelength = wavelengths[channel["wavelength_idx"]]
         table.add_row(
-            label=f'{source_label}_{detector_label} {source_wavelength:.0f}',
-            source=channel['source_idx'],
-            detector=channel['detector_idx'],
-            source_wavelength=source_wavelength
+            label=f"{source_label}_{detector_label} {source_wavelength:.0f}",
+            source=channel["source_idx"],
+            detector=channel["detector_idx"],
+            source_wavelength=source_wavelength,
         )
     table.source.table = sources
     table.detector.table = detectors
@@ -68,27 +90,27 @@ def compile_channels_table(channels_meta, sources, detectors, wavelengths):
 
 def compile_device(channels, manufacturer, nirs_mode):
     return NIRSDevice(
-        name='nirs_device',
-        description='Information about the NIRS device used in this session',
+        name="nirs_device",
+        description="Information about the NIRS device used in this session",
         manufacturer=manufacturer,
         channels=channels,
         sources=channels.source.table,
         detectors=channels.detector.table,
-        nirs_mode=nirs_mode
+        nirs_mode=nirs_mode,
     )
 
 
 def compile_series(timestamps, raw_data, channels):
     return NIRSSeries(
-        name='nirs_data',
-        description='The raw NIRS channel data',
+        name="nirs_data",
+        description="The raw NIRS channel data",
         timestamps=timestamps,
         channels=DynamicTableRegion(
-            name='channels',
-            description='an ordered map to the channels in this NIRS series',
+            name="channels",
+            description="an ordered map to the channels in this NIRS series",
             table=channels,
-            data=channels.id[:]
+            data=channels.id[:],
         ),
         data=raw_data,
-        unit='V'
+        unit="V",
     )
